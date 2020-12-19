f39ac0ef567017970a28624206b8dc5fc97fc4a8
topilski <atopilski@gmail.com>
2020-12-19 10:18:24 +0300

    Style

--- lib/common/controller.dart
+++ lib/common/controller.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
-import 'package:meta/meta.dart';
 import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
 import 'package:player/common/states.dart';
 
 abstract class IPlayerController<T> {
